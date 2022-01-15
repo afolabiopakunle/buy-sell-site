2a68d93bfd2c3f20c479b81a0c3543bc43725d40
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-01-15 19:30:29 +0100

    components creATED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsComponent } from './new-listings/new-listings.component';
+import { EditListingComponent } from './edit-listing/edit-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingsPageComponent
+    ListingsPageComponent,
+    DetailsPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingsComponent,
+    EditListingComponent
   ],
   imports: [
     BrowserModule,
